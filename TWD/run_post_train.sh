d40b80f6178982837b56093940c924720739807f
gminipark <pgm1219@khu.ac.kr>
2024-11-10 12:27:48 +0000

    add a shell file for post-training

---
+++ TWD/run_post_train.sh
@@ -0,0 +1,22 @@
+CUDA_VISIBLE_DEVICES=0,1 accelerate launch --main_process_port 29500 \
+    train.py \
+    --model_name_or_path "facebook/bart-large" \
+    --output_dir "./question_generation/PT-lora/5e-4" \
+    --task_name question_generation \
+    --max_source_length 512 \
+    --per_device_train_batch_size 8  \
+    --per_device_eval_batch_size 4 \
+    --num_train_epochs 10 \
+    --preprocessing_num_workers 4 \
+    --fp16 False \
+    --seed 42 \
+    --predict_with_generate True \
+    --generation_num_beams 4 \
+    --label_smoothing_factor 0.15 \
+    --gradient_accumulation_steps 8 \
+    --learning_rate 5e-4 \
+    --logging_strategy "epoch" \
+    --evaluation_strategy "epoch" \
+    --save_strategy "epoch" \
+    --post_train True \
+    --use_lora True \
