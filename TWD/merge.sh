98173a5b3d1197c276b6c09bd975b9fb0f8fa099
gminipark <pgm1219@khu.ac.kr>
2024-11-10 12:34:38 +0000

    add a shell file for merging weights

---
+++ TWD/merge.sh
@@ -0,0 +1,4 @@
+python merge.py \
+    --base_model facebook/bart-large \
+    --lora_path question_generation/PT-lora/facebook-bart-large/5e-4/checkpoint-2958  \
+    --output_path question_generation/PT-lora/facebook-bart-large/5e-4/checkpoint-e5-merged 
