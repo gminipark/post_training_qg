cba3380c6b4813602a79e7f23c348066709ef60a
gminipark <pgm1219@khu.ac.kr>
2025-11-26 00:45:11 +0000

    add inference shell script

---
+++ CLM/run_inference_clm.sh
@@ -0,0 +1,12 @@
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --main_process_port 29500 \
+    lm_inference_new.py \
+    --task_name question_generation \
+    --max_new_tokens 128 \
+    --checkpoint "./question_generation/FT/gemma-2-9b/lora/fp16/e7/lr5e-5-hl-eos-r32" \
+    --num_proc 4 \
+    --base_model_name "google/gemma-2-9b-it" \
+    --model_type gemma \
+    --batch_size 4 \
+    --num_beams 4 \
+    --use_peft
+    # --min_new_tokens 110 \
