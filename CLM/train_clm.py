9fc820c3c5bf854adceb74536fba6c39a1194093
gminipark <pgm1219@khu.ac.kr>
2025-11-26 04:55:49 +0000

    clean code

--- CLM/train_clm.py
+++ CLM/train_clm.py
@@ -1,12 +1,8 @@
 from transformers import (AutoModelForCausalLM,
                           AutoTokenizer,
-                          TrainingArguments,
-                          BitsAndBytesConfig,
                           set_seed)
 from transformers.trainer_utils import get_last_checkpoint
 from datasets import load_dataset
-from accelerate import Accelerator
-from huggingface_hub import HfFolder
 
 import logging
 import os
@@ -15,12 +11,10 @@ import json
 import evaluate
 import numpy as np
 from dataclasses import dataclass, field
-# from preprocess import (get_preprocess_function)
                         
 from typing import Optional
 
-# from peft import PeftModel, PeftConfig, prepare_model_for_kbit_training, LoraConfig, get_peft_model, TaskType
-from trl import SFTTrainer, SFTConfig, ModelConfig, TrlParser, get_kbit_device_map, get_peft_config, get_quantization_config, setup_chat_format
+from trl import SFTTrainer, SFTConfig, ModelConfig, TrlParser, get_kbit_device_map, get_peft_config, get_quantization_config
 
 logger = logging.getLogger(__name__)
 
