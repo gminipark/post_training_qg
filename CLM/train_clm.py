73c87f003351b3d8a2a2375b51e301fc561c1552
gminipark <pgm1219@khu.ac.kr>
2025-11-26 04:42:52 +0000

    add DataTrainingArguments dataclass and dataset/task column mappings

--- CLM/train_clm.py
+++ CLM/train_clm.py
@@ -23,3 +23,126 @@ from typing import Optional
 from trl import SFTTrainer, SFTConfig, ModelConfig, TrlParser, get_kbit_device_map, get_peft_config, get_quantization_config, setup_chat_format
 
 logger = logging.getLogger(__name__)
+
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
+
+    lang: Optional[str] = field(default=None, metadata={"help": "Language id for summarization."})
+
+    dataset_name: Optional[str] = field(
+        default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."}
+    )
+    task_name: Optional[str] = field(
+        default=None, metadata={"help": "The name of the dataset to use (via the datasets library)."}
+    )
+    source_column: Optional[str] = field(
+        default=None,
+        metadata={"help": "The name of the column in the datasets containing the full texts (for summarization)."},
+    )
+    target_column: Optional[str] = field(
+        default=None,
+        metadata={"help": "The name of the column in the datasets containing the summaries (for summarization)."},
+    )
+    overwrite_cache: bool = field(
+        default=False, metadata={"help": "Overwrite the cached training and evaluation sets"}
+    )
+    data_cache_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": "Where to store the datasets downloaded from huggingface.co"},
+    )
+    preprocessing_num_workers: Optional[int] = field(
+        default=None,
+        metadata={"help": "The number of processes to use for the preprocessing."},
+    )
+
+    pad_to_max_length: bool = field(
+        default=False,
+        metadata={
+            "help": (
+                "Whether to pad all samples to model maximum sentence length. "
+                "If False, will pad the samples dynamically when batching to the maximum length in the batch. More "
+                "efficient on GPU but very bad for TPU."
+            )
+        },
+    )
+    num_beams: Optional[int] = field(
+        default=1,
+        metadata={
+            "help": (
+                "Number of beams to use for evaluation. This argument will be passed to ``model.generate``, "
+                "which is used during ``evaluate`` and ``predict``."
+            )
+        },
+    )
+    ignore_pad_token_for_loss: bool = field(
+        default=True,
+        metadata={
+            "help": "Whether to ignore the tokens corresponding to padded labels in the loss computation or not."
+        },
+    )
+    source_prefix: Optional[str] = field(
+        default=None, metadata={"help": "A prefix to add before every source text (useful for T5 models)."}
+    )
+
+    forced_bos_token: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": (
+                "The token to force as the first generated token after the decoder_start_token_id. "
+                "Useful for multilingual models like mBART where the first generated token"
+                "needs to be the target language token (Usually it is the target language token)"
+            )
+        },
+    )
+    post_train: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": (
+                "Whether to post-train the model"
+            )
+        },
+    )
+    mask_ratio: Optional[float] = field(
+        default=None,
+        metadata={
+            "help": (
+                "The probability with which to (randomly) mask tokens in the input"
+            )
+        },
+    )
+
+    model_type: Optional[str] = field(
+        default="llama",
+        metadata={
+            "help": ("The type of model")
+        }
+    )
+    append_concat_token: Optional[bool] = field(
+        default=False,
+        metadata={"help": "If True, appends `eos_token_id` at the end of each sample being packed."},
+    )
+    add_special_tokens: Optional[bool] = field(
+        default=False,
+        metadata={"help": "If True, tokenizers adds special tokens to each sample being packed."},
+    )
+
+task_to_dataset = {
+                  "news_summarization" : "xsum",
+                  "science_summarization" : "yaolu/multi_x_science_sum",
+                  "conversation_summarization" : "Samsung/samsum",
+                  "question_generation" : "lmqg/qg_squad",
+                  "data_to_text_generation" : "GEM/dart",
+                  "commonsense_generation" : "allenai/common_gen",
+    }
+
+dataset_name_mapping = {
+    "xsum": ("document", "summary"),
+    "yaolu/multi_x_science_sum": ("abstract", "related_work"),
+    "Samsung/samsum": ("dialogue", "summary"),
+    "lmqg/qg_squad": ("paragraph_answer", "question"),
+    "GEM/dart": ("tripleset", "target"),
+    "allenai/common_gen": ("concepts", "target"),
+}
