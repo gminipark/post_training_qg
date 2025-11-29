a4b340a4b44b3edd1d5d1c80e39d9af7f1120ba2
gminipark <pgm1219@khu.ac.kr>
2025-11-29 12:03:14 +0000

    add shell for environment

---
+++ TWD/setup.sh
@@ -0,0 +1,20 @@
+pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+
+pip install transformers==4.51.3
+pip install evaluate
+pip install datasets==3.6.0
+pip install peft
+pip install accelerate
+pip install deepspeed
+pip install sentencepiece
+pip install nltk
+pip install bitsandbytes
+pip install tensorboardX
+
+
+pip install sacrebleu
+pip install bert-score
+pip install rouge_score
+
+pip install "unsloth[cu124-torch251] @ git+https://github.com/unslothai/unsloth.git"
+pip install unsloth_zoo
